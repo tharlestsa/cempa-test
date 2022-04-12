2c1bdf9d8b6d7dcde457b14f877a3184267963f2
Jairo Matos da Rocha <jairo@discente.ufg.br>
2022-04-12 10:22:31 -0300

    como fiz isso, nao sei so sei que deu certo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import Map from 'ol/Map';
 import View from 'ol/View';
+import BaseObject from 'ol/Object';
 import TileLayer from 'ol/layer/Tile';
 import Overlay from 'ol/Overlay';
 import VectorLayer from "ol/layer/Vector";
@@ -41,7 +42,7 @@ export class AppComponent implements OnInit {
   map: Map | undefined;
 
   items: MenuItem[];
-  layer: TileLayer<any> | undefined;
+  layer: Array<TileLayer<any>> | undefined;
   image: Image<any> | undefined;
   idw: IDW;
   serie: any[];
@@ -76,18 +77,247 @@ export class AppComponent implements OnInit {
     this.items = [
       {label: 'Temperaturas', icon: 'pi pi-fw pi-circle-fill',  command: event => this.changeLayer(3)}
     ];
-    this.layersMapserver = [
-      "td2mj__2022-04-08T09-00",
-      "td2mj__2022-04-08T13-00",
-      "td2mj__2022-04-08T23-00",
-      "td2mj__2022-04-09T08-00",
-      "td2mj__2022-04-09T19-00",
-      "td2mj__2022-04-11T02-00",
-      "td2mj__2022-04-12T05-00",
-      "td2mj__2022-04-12T13-00",
-      "td2mj__2022-04-12T15-00",
-      "td2mj__2022-04-12T21-00"
-    ];
+    this.layersMapserver = ['t2mj__2022-04-11T00-00',
+    't2mj__2022-04-11T01-00',
+    't2mj__2022-04-11T02-00',
+    't2mj__2022-04-11T03-00',
+    't2mj__2022-04-11T04-00',
+    't2mj__2022-04-11T05-00',
+    't2mj__2022-04-11T06-00',
+    't2mj__2022-04-11T07-00',
+    't2mj__2022-04-11T08-00',
+    't2mj__2022-04-11T09-00',
+    't2mj__2022-04-11T10-00',
+    't2mj__2022-04-11T11-00',
+    't2mj__2022-04-11T12-00',
+    't2mj__2022-04-11T13-00',
+    't2mj__2022-04-11T14-00',
+    't2mj__2022-04-11T15-00',
+    't2mj__2022-04-11T16-00',
+    't2mj__2022-04-11T17-00',
+    't2mj__2022-04-11T18-00',
+    't2mj__2022-04-11T19-00',
+    't2mj__2022-04-11T20-00',
+    't2mj__2022-04-11T21-00',
+    't2mj__2022-04-11T22-00',
+    't2mj__2022-04-11T23-00',
+    't2mj__2022-04-12T00-00',
+    't2mj__2022-04-12T01-00',
+    't2mj__2022-04-12T02-00',
+    't2mj__2022-04-12T03-00',
+    't2mj__2022-04-12T04-00',
+    't2mj__2022-04-12T05-00',
+    't2mj__2022-04-12T06-00',
+    't2mj__2022-04-12T07-00',
+    't2mj__2022-04-12T08-00',
+    't2mj__2022-04-12T09-00',
+    't2mj__2022-04-12T10-00',
+    't2mj__2022-04-12T11-00',
+    't2mj__2022-04-12T12-00',
+    't2mj__2022-04-12T13-00',
+    't2mj__2022-04-12T14-00',
+    't2mj__2022-04-12T15-00',
+    't2mj__2022-04-12T16-00',
+    't2mj__2022-04-12T17-00',
+    't2mj__2022-04-12T18-00',
+    't2mj__2022-04-12T19-00',
+    't2mj__2022-04-12T20-00',
+    't2mj__2022-04-12T21-00',
+    't2mj__2022-04-12T22-00',
+    't2mj__2022-04-12T23-00',
+    't2mj__2022-04-13T00-00',
+    't2mj__2022-04-13T01-00',
+    't2mj__2022-04-13T02-00',
+    't2mj__2022-04-13T03-00',
+    't2mj__2022-04-13T04-00',
+    't2mj__2022-04-13T05-00',
+    't2mj__2022-04-13T06-00',
+    't2mj__2022-04-13T07-00',
+    't2mj__2022-04-13T08-00',
+    't2mj__2022-04-13T09-00',
+    't2mj__2022-04-13T10-00',
+    't2mj__2022-04-13T11-00',
+    't2mj__2022-04-13T12-00',
+    't2mj__2022-04-13T13-00',
+    't2mj__2022-04-13T14-00',
+    't2mj__2022-04-13T15-00',
+    't2mj__2022-04-13T16-00',
+    't2mj__2022-04-13T17-00',
+    't2mj__2022-04-13T18-00',
+    't2mj__2022-04-13T19-00',
+    't2mj__2022-04-13T20-00',
+    't2mj__2022-04-13T21-00',
+    't2mj__2022-04-13T22-00',
+    't2mj__2022-04-13T23-00',
+    't2mj__2022-04-14T00-00',
+    't2mj__2022-04-14T01-00',
+    't2mj__2022-04-14T02-00',
+    't2mj__2022-04-14T03-00',
+    't2mj__2022-04-14T04-00',
+    't2mj__2022-04-14T05-00',
+    't2mj__2022-04-14T06-00',
+    't2mj__2022-04-14T07-00',
+    't2mj__2022-04-14T08-00',
+    't2mj__2022-04-14T09-00',
+    't2mj__2022-04-14T10-00',
+    't2mj__2022-04-14T11-00',
+    't2mj__2022-04-14T12-00',
+    't2mj__2022-04-14T13-00',
+    't2mj__2022-04-14T14-00',
+    't2mj__2022-04-14T15-00',
+    't2mj__2022-04-14T16-00',
+    't2mj__2022-04-14T17-00',
+    't2mj__2022-04-14T18-00',
+    't2mj__2022-04-14T19-00',
+    't2mj__2022-04-14T20-00',
+    't2mj__2022-04-14T21-00',
+    't2mj__2022-04-14T22-00',
+    't2mj__2022-04-14T23-00',
+    't2mj__2022-04-15T00-00',
+    't2mj__2022-04-15T01-00',
+    't2mj__2022-04-15T02-00',
+    't2mj__2022-04-15T03-00',
+    't2mj__2022-04-15T04-00',
+    't2mj__2022-04-15T05-00',
+    't2mj__2022-04-15T06-00',
+    't2mj__2022-04-15T07-00',
+    't2mj__2022-04-15T08-00',
+    't2mj__2022-04-15T09-00',
+    't2mj__2022-04-15T10-00',
+    't2mj__2022-04-15T11-00',
+    't2mj__2022-04-15T12-00',
+    't2mj__2022-04-15T13-00',
+    't2mj__2022-04-15T14-00',
+    't2mj__2022-04-15T15-00',
+    't2mj__2022-04-15T16-00',
+    't2mj__2022-04-15T17-00',
+    't2mj__2022-04-15T18-00',
+    't2mj__2022-04-15T19-00',
+    't2mj__2022-04-15T20-00',
+    't2mj__2022-04-15T21-00',
+    't2mj__2022-04-15T22-00',
+    't2mj__2022-04-15T23-00',
+    't2mj__2022-04-16T00-00',
+    't2mj__2022-04-16T01-00',
+    't2mj__2022-04-16T02-00',
+    't2mj__2022-04-16T03-00',
+    't2mj__2022-04-16T04-00',
+    't2mj__2022-04-16T05-00',
+    't2mj__2022-04-16T06-00',
+    't2mj__2022-04-16T07-00',
+    't2mj__2022-04-16T08-00',
+    't2mj__2022-04-16T09-00',
+    't2mj__2022-04-16T10-00',
+    't2mj__2022-04-16T11-00',
+    't2mj__2022-04-16T12-00',
+    't2mj__2022-04-16T13-00',
+    't2mj__2022-04-16T14-00',
+    't2mj__2022-04-16T15-00',
+    't2mj__2022-04-16T16-00',
+    't2mj__2022-04-16T17-00',
+    't2mj__2022-04-16T18-00',
+    't2mj__2022-04-16T19-00',
+    't2mj__2022-04-16T20-00',
+    't2mj__2022-04-16T21-00',
+    't2mj__2022-04-16T22-00',
+    't2mj__2022-04-16T23-00',
+    't2mj__2022-04-17T00-00',
+    't2mj__2022-04-17T01-00',
+    't2mj__2022-04-17T02-00',
+    't2mj__2022-04-17T03-00',
+    't2mj__2022-04-17T04-00',
+    't2mj__2022-04-17T05-00',
+    't2mj__2022-04-17T06-00',
+    't2mj__2022-04-17T07-00',
+    't2mj__2022-04-17T08-00',
+    't2mj__2022-04-17T09-00',
+    't2mj__2022-04-17T10-00',
+    't2mj__2022-04-17T11-00',
+    't2mj__2022-04-17T12-00',
+    't2mj__2022-04-17T13-00',
+    't2mj__2022-04-17T14-00',
+    't2mj__2022-04-17T15-00',
+    't2mj__2022-04-17T16-00',
+    't2mj__2022-04-17T17-00',
+    't2mj__2022-04-17T18-00',
+    't2mj__2022-04-17T19-00',
+    't2mj__2022-04-17T20-00',
+    't2mj__2022-04-17T21-00',
+    't2mj__2022-04-17T22-00',
+    't2mj__2022-04-17T23-00',
+    't2mj__2022-04-18T00-00',
+    't2mj__2022-04-18T01-00',
+    't2mj__2022-04-18T02-00',
+    't2mj__2022-04-18T03-00',
+    't2mj__2022-04-18T04-00',
+    't2mj__2022-04-18T05-00',
+    't2mj__2022-04-18T06-00',
+    't2mj__2022-04-18T07-00',
+    't2mj__2022-04-18T08-00',
+    't2mj__2022-04-18T09-00',
+    't2mj__2022-04-18T10-00',
+    't2mj__2022-04-18T11-00',
+    't2mj__2022-04-18T12-00',
+    't2mj__2022-04-18T13-00',
+    't2mj__2022-04-18T14-00',
+    't2mj__2022-04-18T15-00',
+    't2mj__2022-04-18T16-00',
+    't2mj__2022-04-18T17-00',
+    't2mj__2022-04-18T18-00',
+    't2mj__2022-04-18T19-00',
+    't2mj__2022-04-18T20-00',
+    't2mj__2022-04-18T21-00',
+    't2mj__2022-04-18T22-00',
+    't2mj__2022-04-18T23-00',
+    't2mj__2022-04-19T00-00',
+    't2mj__2022-04-19T01-00',
+    't2mj__2022-04-19T02-00',
+    't2mj__2022-04-19T03-00',
+    't2mj__2022-04-19T04-00',
+    't2mj__2022-04-19T05-00',
+    't2mj__2022-04-19T06-00',
+    't2mj__2022-04-19T07-00',
+    't2mj__2022-04-19T08-00',
+    't2mj__2022-04-19T09-00',
+    't2mj__2022-04-19T10-00',
+    't2mj__2022-04-19T11-00',
+    't2mj__2022-04-19T12-00',
+    't2mj__2022-04-19T13-00',
+    't2mj__2022-04-19T14-00',
+    't2mj__2022-04-19T15-00',
+    't2mj__2022-04-19T16-00',
+    't2mj__2022-04-19T17-00',
+    't2mj__2022-04-19T18-00',
+    't2mj__2022-04-19T19-00',
+    't2mj__2022-04-19T20-00',
+    't2mj__2022-04-19T21-00',
+    't2mj__2022-04-19T22-00',
+    't2mj__2022-04-19T23-00',
+    't2mj__2022-04-20T00-00',
+    't2mj__2022-04-20T01-00',
+    't2mj__2022-04-20T02-00',
+    't2mj__2022-04-20T03-00',
+    't2mj__2022-04-20T04-00',
+    't2mj__2022-04-20T05-00',
+    't2mj__2022-04-20T06-00',
+    't2mj__2022-04-20T07-00',
+    't2mj__2022-04-20T08-00',
+    't2mj__2022-04-20T09-00',
+    't2mj__2022-04-20T10-00',
+    't2mj__2022-04-20T11-00',
+    't2mj__2022-04-20T12-00',
+    't2mj__2022-04-20T13-00',
+    't2mj__2022-04-20T14-00',
+    't2mj__2022-04-20T15-00',
+    't2mj__2022-04-20T16-00',
+    't2mj__2022-04-20T17-00',
+    't2mj__2022-04-20T18-00',
+    't2mj__2022-04-20T19-00',
+    't2mj__2022-04-20T20-00',
+    't2mj__2022-04-20T21-00',
+    't2mj__2022-04-20T22-00',
+    't2mj__2022-04-20T23-00',
+    't2mj__2022-04-21T00-00'];
 
     this.selectedLayerMapserver = this.layersMapserver[0];
     this.isStopped = false;
@@ -96,17 +326,8 @@ export class AppComponent implements OnInit {
 
   ngOnInit(): void {
     this.primengConfig.ripple = true;
-    this.layer = new TileLayer({
-      opacity: 0.6,
-      source: new XYZ({
-        urls: [
-          `https://o9.lapig.iesa.ufg.br/ows?layers=${this.selectedLayerMapserver}&mode=tile&tile={x}+{y}+{z}&tilemode=gmap&map.imagetype=png`,
-          `https://o10.lapig.iesa.ufg.br/ows?layers=${this.selectedLayerMapserver}&mode=tile&tile={x}+{y}+{z}&tilemode=gmap&map.imagetype=png`,
-          `https://o11.lapig.iesa.ufg.br/ows?layers=${this.selectedLayerMapserver}&mode=tile&tile={x}+{y}+{z}&tilemode=gmap&map.imagetype=png`,
-          `https://o12.lapig.iesa.ufg.br/ows?layers=${this.selectedLayerMapserver}&mode=tile&tile={x}+{y}+{z}&tilemode=gmap&map.imagetype=png`,
-        ]
-      })
-    });
+    
+    
     let extent = [-55.0000000000000000,-21.0000000000000000, -42.9614372253417969,-8.9754829406738299];
     const wordExtent: Array<number> = [-122.19, -59.87, -25.28, 32.72];
     ol_get('EPSG:4674').setExtent(extent);
@@ -121,6 +342,7 @@ export class AppComponent implements OnInit {
 
     this.map = new Map({
       view: new View({
+        
         center: Proj.fromLonLat([-49.624, -16.042], 'EPSG:4674'),
         zoom: 1,
         extent: extent,
@@ -128,12 +350,17 @@ export class AppComponent implements OnInit {
       }),
       layers: [
         new TileLayer({
+          properties:{
+            type:'base'
+          },
           source: new OSM(),
         }),
-        this.layer,
+        
         new TileLayer({
-          
-          opacity: 0.7,
+          properties:{
+            type:'base'
+          },
+          opacity: 0.9,
           source: new XYZ({
             urls: [
               `https://o1.lapig.iesa.ufg.br/ows?layers=municipios_goias&mode=tile&tile={x}+{y}+{z}&tilemode=gmap&map.imagetype=png`,
@@ -147,6 +374,10 @@ export class AppComponent implements OnInit {
       target: 'ol-map'
     });
 
+
+    for(let i = 0;i < this.layersMapserver.length;i++ ){
+      this.map?.addLayer(this.creatLayer(i))
+    }
 
     const self = this;
     this.map.on('singleclick', function (evt) {
@@ -209,29 +440,61 @@ export class AppComponent implements OnInit {
     // })
   }
 
-  changeLayer(layer: number){
+  creatLayer(layer: number){
     this.selectedLayerMapserver = this.layersMapserver[layer]
-    this.layer?.getSource().setUrls(
-      [
-        `https://o9.lapig.iesa.ufg.br/ows?layers=${this.selectedLayerMapserver}&mode=tile&tile={x}+{y}+{z}&tilemode=gmap&map.imagetype=png`,
-        `https://o10.lapig.iesa.ufg.br/ows?layers=${this.selectedLayerMapserver}&mode=tile&tile={x}+{y}+{z}&tilemode=gmap&map.imagetype=png`,
-        `https://o11.lapig.iesa.ufg.br/ows?layers=${this.selectedLayerMapserver}&mode=tile&tile={x}+{y}+{z}&tilemode=gmap&map.imagetype=png`,
-        `https://o12.lapig.iesa.ufg.br/ows?layers=${this.selectedLayerMapserver}&mode=tile&tile={x}+{y}+{z}&tilemode=gmap&map.imagetype=png`,
-      ]
-    );
+    let visible = false
+    let opacity = 0.0
+    
+    if (layer < 8){
+      visible =  true
+      opacity = 0.0
+    }
+    if (layer == 0){
+      visible =  true
+      opacity = 0.8
+    }
+    return new TileLayer({
+      properties:{
+        key: layer,
+        label: this.selectedLayerMapserver,
+        visible: visible
+      },
+      opacity: opacity,
+      visible: visible,
+      source: new XYZ({
+        urls: [
+          `https://o9.lapig.iesa.ufg.br/ows?layers=${this.selectedLayerMapserver}&mode=tile&tile={x}+{y}+{z}&tilemode=gmap&map.imagetype=png`,
+          `https://o10.lapig.iesa.ufg.br/ows?layers=${this.selectedLayerMapserver}&mode=tile&tile={x}+{y}+{z}&tilemode=gmap&map.imagetype=png`,
+          `https://o11.lapig.iesa.ufg.br/ows?layers=${this.selectedLayerMapserver}&mode=tile&tile={x}+{y}+{z}&tilemode=gmap&map.imagetype=png`,
+          `https://o12.lapig.iesa.ufg.br/ows?layers=${this.selectedLayerMapserver}&mode=tile&tile={x}+{y}+{z}&tilemode=gmap&map.imagetype=png`,
+        ]
+      })
+    });
+
+  }
+
+  changeLayer(key: number){
+    this.map?.getLayers().forEach(layer => {
+      let tmpkey = key['value'] 
+      if (layer.get('key') > tmpkey - 7  &&   layer.get('key') < tmpkey + 7    || layer.get('type') === 'base' ){
+        if (layer.get('key') === tmpkey || layer.get('type') === 'base' ) {
+          layer.setVisible(true)
+          layer.setOpacity(0.8)
+        }else{
+          layer.setVisible(true)
+          layer.setOpacity(0.0)
+        }
+      }else{
+        layer.setVisible(false)
+      }
+      
+    });
    
   }
 
   onDateChange(ev: any){
     this.selectedLayerMapserver = this.layersMapserver[ev.value];
-    this.layer?.getSource().setUrls(
-      [
-        `https://o9.lapig.iesa.ufg.br/ows?layers=${this.selectedLayerMapserver}&mode=tile&tile={x}+{y}+{z}&tilemode=gmap&map.imagetype=png`,
-        `https://o10.lapig.iesa.ufg.br/ows?layers=${this.selectedLayerMapserver}&mode=tile&tile={x}+{y}+{z}&tilemode=gmap&map.imagetype=png`,
-        `https://o11.lapig.iesa.ufg.br/ows?layers=${this.selectedLayerMapserver}&mode=tile&tile={x}+{y}+{z}&tilemode=gmap&map.imagetype=png`,
-        `https://o12.lapig.iesa.ufg.br/ows?layers=${this.selectedLayerMapserver}&mode=tile&tile={x}+{y}+{z}&tilemode=gmap&map.imagetype=png`,
-      ]
-    );
+    this.changeLayer(ev)
   }
 
   setGeojson(){
@@ -252,7 +515,7 @@ export class AppComponent implements OnInit {
       console.log(this.idw)
       this.image = new Image({
         source: this.idw,
-        opacity: .5
+        opacity: .0
       });
 
       this.map?.addLayer(this.image);
@@ -271,16 +534,17 @@ export class AppComponent implements OnInit {
         }
         this.onDateChange({value: this.date})
       }
-    }, 1000);
+    }, 500);
   }
 
   stop(){
     this.isStopped = true;
+    console.log(this.map?.getLayers())
   }
 
   formatDate(date): string {
     let string = ''; 
-    date = date.replace('td2mj__', '').split("T");
+    date = date.replace('t2mj__', '').split("T");
     let dt = date[0]
     let time = date[1].replace('-', ':')  
     return dt + ' ' + time;
